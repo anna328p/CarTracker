a84252114c34a568decbb3c245c9f6cddfe51541
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2016-08-22 13:08:23 -0700

    Lots of stuff modified to start adding logic

--- app/src/main/java/xyz/dkudriavtsev/cartracker/Vehicles.java
+++ app/src/main/java/xyz/dkudriavtsev/cartracker/Vehicles.java
@@ -1,6 +1,5 @@
 package xyz.dkudriavtsev.cartracker;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class Home extends AppCompatActivity
+public class Vehicles extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_vehicles);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,7 +27,8 @@ public class Home extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), AddItem.class));
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
@@ -55,7 +55,7 @@ public class Home extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.vehicles, menu);
         return true;
     }
 
@@ -68,7 +68,7 @@ public class Home extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            startActivity(new Intent(getApplicationContext(), Settings.class));
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -80,13 +80,17 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if        (id == R.id.nav_home)   {
-            startActivity(new Intent(getApplicationContext(), Home.class));
-        } else if (id == R.id.nav_vehicles)  {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_categories){
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_data)   {
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
 
         }
 
