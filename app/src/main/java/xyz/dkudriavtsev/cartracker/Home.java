7a255baeb9ca727fbeb4adc02cd47f84341c9934
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2016-08-22 10:47:50 -0700

    Created necessary navigation bar controls. BROKEN.

--- app/src/main/java/xyz/dkudriavtsev/cartracker/Home.java
+++ app/src/main/java/xyz/dkudriavtsev/cartracker/Home.java
@@ -80,17 +80,15 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if        (id == R.id.nav_home)   {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_vehicles)  {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_categories){
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_data)   {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings)    {
 
         }
 
